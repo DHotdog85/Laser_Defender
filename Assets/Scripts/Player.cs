38d8e7ac4314c38918daaa932be591d4304fbfeb
Don H <dhayes506@hotmail.com>
2020-09-25 20:39:52 -0400

    Set up player movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float xPadding = 0.5f;
+    [SerializeField] private float yPadding = 0.5f;
+    
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - xPadding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + yPadding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - yPadding;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPosition = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPosition,newYPosition);
+    }
+}
